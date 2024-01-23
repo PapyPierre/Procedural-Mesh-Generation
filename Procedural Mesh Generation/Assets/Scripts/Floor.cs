ae0437eaf3819468169de44957ea3d430261dcff
Papy Pierre <pierreferrari22@gmail.com>
2024-01-23 19:51:47 +0100

    Class Floor, vertices

---
+++ Procedural Mesh Generation/Assets/Scripts/Floor.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Floor
+{
+    public Vector3 anchorPos;
+    public float radius;
+    public Color color;
+}
--- Procedural Mesh Generation/Assets/Scripts/IslandMeshGenerator.cs
+++ Procedural Mesh Generation/Assets/Scripts/IslandMeshGenerator.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 [ExecuteAlways]
@@ -10,71 +10,145 @@ public class IslandMeshGenerator : MonoBehaviour
 {
     [SerializeField] private bool showGizmos;
 
-    private readonly List<Vector3> _floorsAnchorPos = new List<Vector3>();
+    [Header("Seed")] [SerializeField, ReadOnly]
+    private uint islandSeed;
+
+    [SerializeField, HideIf("randomSpeed")]
+    private uint customIslandSeed;
+
+    [SerializeField] private bool randomSpeed;
+
+    private readonly List<Floor> _floors = new List<Floor>();
     private Vector3 tipPos;
 
-    [SerializeField, Header("Floors parameters")] private int floorCount;
-    [SerializeField] private float floorsSpacing;
-    [SerializeField] private float floorsRadius;
-    [SerializeField] private float floorsMarging;
-    
-    [SerializeField, Range(0.001f, 1f), Space] private float upwardSwelling;
+    [SerializeField, Header("Floors parameters")]
+    private int floorCount;
 
-    [SerializeField, Tooltip("Number of vertices per floor")] private int verticesPerFloor;
+    [SerializeField] private Gradient floorColorGradient;
+    [SerializeField] private float minFloorsHeight;
+    [SerializeField] private float maxFloorsHeight;
+    [SerializeField] private float minFloorsRadius;
+    [SerializeField] private float maxFloorsRadius;
+    [SerializeField] private float floorsMaxOffset;
+
+    [SerializeField, Space] private float upwardSwelling;
+    [SerializeField] private float upwardSwellingPow;
+
+    [SerializeField, Tooltip("Number of vertices per floor")]
+    private int verticesPerFloor;
+
     private readonly List<Vector3> _verticesPos = new List<Vector3>();
     [SerializeField] private int minVerticesSpacing;
     [SerializeField] private int maxVerticesSpacing;
 
+    [SerializeField] private float verticesGizmosSize;
+    [SerializeField] private Color verticesGizmosColor;
+
     [Button()]
     private void GenerateIsland()
     {
+        if (randomSpeed) islandSeed = (uint)Random.Range(0, 9999999);
+        else islandSeed = customIslandSeed;
+        SRnd.SetSeed(islandSeed);
+
         DestroyPreviousIsland();
-        FindFloorsAnchorPos();
+        SetUpFloors();
         FindTipPos();
         FindIslandVertices();
     }
 
     private void DestroyPreviousIsland()
     {
-        _floorsAnchorPos.Clear();
+        _floors.Clear();
         _verticesPos.Clear();
     }
-    private void FindFloorsAnchorPos()
+
+    private void SetUpFloors()
     {
         for (int i = 0; i < floorCount; i++)
         {
-            _floorsAnchorPos.Add(new Vector3(0,-i * floorsSpacing, 0));
+            _floors.Add( new Floor());
+        }
+
+        for (var index = 0; index < _floors.Count; index++)
+        {
+            Floor floor = _floors[index];
+            float x = (float)index / _floors.Count;
+            floor.color = floorColorGradient.Evaluate(x);
+            SetUpFloorAnchor(floor, index);
+            SetUpFloorRadius(floor, index);
         }
     }
+
+    private void SetUpFloorAnchor(Floor newFloor, int i)
+    {
+        Vector3 randomPos = GetRandomFloorPos();
+        
+        float y;
+
+        if (i > 0)
+        {
+            Vector3 previousFloorAnchorPos = _floors[i - 1].anchorPos;
+            y = previousFloorAnchorPos.y - randomPos.y;
+        }
+        else y = -randomPos.y;
+        
+        newFloor.anchorPos = new Vector3(randomPos.x, y, randomPos.z);
+    }
+
+    private void SetUpFloorRadius(Floor newFloor, int i)
+    {
+        float upwardSwellingValue = Mathf.Pow(_floors.Count - i, upwardSwellingPow) * upwardSwelling;
+        float tempRadius = SRnd.RangeFloat(minFloorsRadius, maxFloorsRadius);
+        float radius = tempRadius * upwardSwellingValue;
+        newFloor.radius = radius;
+    }
+
     private void FindTipPos()
     {
-        tipPos = new Vector3(0,-floorCount * floorsSpacing, 0);
+        Vector3 randomPos = GetRandomFloorPos();
+        Vector3 lastAnchorPos = _floors[^1].anchorPos;
+        float y = lastAnchorPos.y - randomPos.y;
+        tipPos = new Vector3(randomPos.x /_floors.Count, y, randomPos.z /_floors.Count);
     }
+
+    private Vector3 GetRandomFloorPos()
+    {
+        float height = SRnd.RangeFloat(minFloorsHeight, maxFloorsHeight);
+        float xOffset = SRnd.RangeFloat(-floorsMaxOffset, floorsMaxOffset);
+        float zOffset = SRnd.RangeFloat(-floorsMaxOffset, floorsMaxOffset);
+
+        return new Vector3(xOffset, height, zOffset);
+    }
+
     private void FindIslandVertices()
     {
-        //BUG Does not work
-        
-        float distanceWithinMarge = Random.Range(floorsRadius - floorsMarging, floorsRadius + floorsMarging) * upwardSwelling;
-
-        for (int i = 0; i < verticesPerFloor; i++)
+        foreach (var floor in _floors)
         {
-            float angle = Random.Range(0f, 2 * Mathf.PI);
+            var floorRadius = floor.radius;
             
-            float x = distanceWithinMarge * Mathf.Cos(angle);
-            float y = distanceWithinMarge * Mathf.Sin(angle);
-            float z = distanceWithinMarge * Mathf.Sin(angle);
-            
-            
-            _verticesPos.Add(new Vector3(x,y, z));
+            for (int i = 0; i < verticesPerFloor; i++)
+            {
+                Vector2 randomPosInCircle = SRnd.GenerateRandomPointInsideUnitCircle() * floorRadius * SRnd.RangeFloat(0, 1);
+
+                float x = randomPosInCircle.x + floor.anchorPos.x;
+                float y = floor.anchorPos.y;
+                float z = randomPosInCircle.y + floor.anchorPos.z;
+                
+                _verticesPos.Add(new Vector3(x, y, z));
+            }
         }
     }
-    
+
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
         if (!showGizmos) return;
-        if (_floorsAnchorPos.Count < 1) return;
-
+        if (_floors.Count < 1) return;
+        
+        // White center line
+        Handles.DrawLine(Vector3.zero, new Vector3(0,-200,0));
+        
         DrawFloorsRadius();
         DrawTip();
         DrawVertices();
@@ -82,42 +156,28 @@ public class IslandMeshGenerator : MonoBehaviour
 
     private void DrawFloorsRadius()
     {
-        for (var i = 0; i < _floorsAnchorPos.Count; i++)
+        Handles.DrawWireDisc(_floors[0].anchorPos, Vector3.up, _floors[0].radius, 3);
+
+        for (var i = 0; i < _floors.Count; i++)
         {
-            Vector3 pos = _floorsAnchorPos[i];
-
-            float upwardSwellingValue = (_floorsAnchorPos.Count - i) * upwardSwelling;
-            float r = floorsRadius * upwardSwellingValue;
-            float margedRadius = floorsMarging  * upwardSwellingValue;
-            
-            Handles.color = Color.yellow;
-            
-            // Inner disc
-            Handles.DrawWireDisc(pos, Vector3.up, r - margedRadius, 1);
-            
-            // Outer disc
-            Handles.DrawWireDisc(pos, Vector3.up, r + margedRadius, 1);
-            
-            // Upper Disc
-            Handles.DrawWireDisc(new Vector3(pos.x, pos.y + margedRadius, pos.z), Vector3.up, r, 1);
-            
-            // Lower Disc
-            Handles.DrawWireDisc(new Vector3(pos.x, pos.y - margedRadius, pos.z), Vector3.up, r, 1);
-
-            Handles.color = Color.red;
-            Handles.DrawWireDisc(pos, Vector3.up, r, 3);
+            Floor floor = _floors[i];
+            Handles.color = floor.color;
+            Handles.DrawWireDisc(floor.anchorPos, Vector3.up, floor.radius, 3);
         }
     }
+
     private void DrawTip()
     {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawSphere(tipPos, 0.2f);
+        Gizmos.color = verticesGizmosColor;
+        Gizmos.DrawSphere(tipPos, verticesGizmosSize);
     }
+
     private void DrawVertices()
     {
         foreach (var pos in _verticesPos)
         {
-            Gizmos.DrawSphere(pos, 0.2f);
+            Gizmos.color = verticesGizmosColor;
+            Gizmos.DrawSphere(pos, verticesGizmosSize);
         }
     }
 #endif
